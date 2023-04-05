fc825fbc95a948a3377a01dfde0795509306f76c
BrenoHB <111541433+BrenoHB@users.noreply.github.com>
2023-04-05 19:58:00 -0300

    Ajuste
    aa

---
+++ ambiente-docker-php-nginx-mariadb-main/public/banco.php
@@ -0,0 +1,21 @@
+<?php
+
+// try{
+    
+$pdo = new PDO("mysql:host=mariadb;dbname=trabalhofinal;","root", "mariadb");
+
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// // echo "Conexão bem sucedida!";
+
+// }catch(PDOException $e) {
+//     echo "Falha na conexão: " . $e->getMessage();
+// }
+
+$senha = md5($_POST['senha']);
+
+$sql = $pdo->prepare("INSERT INTO user (nome, senha) VALUES (?, ?)");
+$sql->execute(array($_POST['nome'],$senha));
+
+// $pdo->execute('DELETE FROM user;');
+?>
+
--- ambiente-docker-php-nginx-mariadb-main/public/index.php
+++ ambiente-docker-php-nginx-mariadb-main/public/index.php
@@ -1,3 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <style>
+        .form{
+            width: 300px;
+            height: 300px;
+            background: #fff;
+        }
+        .box{
+            width: 100vw;
+            height: 100vh;
+            background: ;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+        }
+        body {
+            margin: 0px;
+        }
+        .php{
+            width: 100vw;
+            height: 100vh;
+            background: #6C7A89;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Fazer Login</h1>
+    <br>
+    <div class="box">
+        <div class="form">
+            <form method="post" action="banco.php">
+                <br>
+                <label for="user">Usuario: </label><br>
+                <input type="text" name="nome" id="user"><br>
+                <label for="pass">Senha: </label><br>
+                <input type="password" name="senha" id="pass"><br>
+                <button type="submit">enviar</button>
+                <br>
+            </form>
+        </div>
+    </div>
+    <div class="php">
+    <?php
 
-echo phpinfo();
+    ?>
+    </div>
+</body>
+</html>
